ebb8040cffc5d10b779be9fcb9d7c4be1216aaa0
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-03-17 12:53:36 +0100

    Setup

---
+++ test/e2e.js
@@ -0,0 +1,11 @@
+let childProcess = require('child_process');
+let exec = childProcess.exec;
+
+describe('Basic tests', () => {
+  beforeEach((done) => {
+    done();
+  });
+  afterEach((done) => {
+    done();
+  });
+});
