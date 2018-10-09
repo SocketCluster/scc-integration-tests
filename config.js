37c1ddb995a27c4ba28e5a6cbbe2e2090de151c4
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-10-10 01:52:29 +0200

    Bump SCC components

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ let path = require('path');
 module.exports = {
   state: {
     imageName: 'socketcluster/scc-state',
-    versionTag: 'v6.1.0',
+    versionTag: 'v6.1.1',
     internalContainerPort: 7777
   },
   worker: {
     imageName: 'socketcluster/socketcluster',
-    versionTag: 'v14.0.4',
+    versionTag: 'v14.3.1',
     internalContainerPort: 8000
   },
   broker: {
     imageName: 'socketcluster/scc-broker',
-    versionTag: 'v6.0.1',
+    versionTag: 'v6.0.2',
     internalContainerPort: 8888
   },
   subscriberInstancePath: path.resolve(__dirname, 'client-subscribers.js'),
