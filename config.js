b3f1c655592e24f4cfafd4dddef92e8612d38ed7
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-03-02 20:40:15 +0100

    v5.3.2

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ let path = require('path');
 module.exports = {
   state: {
     imageName: 'socketcluster/agc-state',
-    versionTag: 'v5.3.0',
+    versionTag: 'v5.3.1',
     internalContainerPort: 7777
   },
   worker: {
     imageName: 'socketcluster/asyngular',
-    versionTag: 'v5.3.1',
+    versionTag: 'v5.3.2',
     internalContainerPort: 8000
   },
   broker: {
     imageName: 'socketcluster/agc-broker',
-    versionTag: 'v5.3.0',
+    versionTag: 'v5.3.1',
     internalContainerPort: 8888
   },
   subscriberInstancePath: path.resolve(__dirname, 'client-subscribers.js'),
