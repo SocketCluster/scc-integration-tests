e1556f25d71bdd45a44eda4114b004eef2fd87fc
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-03-17 23:46:43 +0100

    v5.3.4

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   worker: {
     imageName: 'socketcluster/asyngular',
-    versionTag: 'v5.3.3',
+    versionTag: 'v5.3.4',
     internalContainerPort: 8000
   },
   broker: {
