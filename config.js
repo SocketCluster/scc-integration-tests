4867049852ec83897114caf9926c5e094771c785
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2020-02-07 22:01:16 +0100

    v8.0.0

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ let path = require('path');
 module.exports = {
   state: {
     imageName: 'socketcluster/scc-state',
-    versionTag: 'v7.0.0',
+    versionTag: 'v8.0.2',
     internalContainerPort: 7777
   },
   worker: {
     imageName: 'socketcluster/socketcluster',
-    versionTag: 'v15.0.6',
+    versionTag: 'v16.0.1',
     internalContainerPort: 8000
   },
   broker: {
     imageName: 'socketcluster/scc-broker',
-    versionTag: 'v7.0.0',
+    versionTag: 'v8.0.0',
     internalContainerPort: 8888
   },
   subscriberInstancePath: path.resolve(__dirname, 'client-subscribers.js'),
